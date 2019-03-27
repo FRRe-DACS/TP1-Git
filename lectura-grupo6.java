1f40759345cb5ea985dbe4c9ca9f95bbf27e7e9a
Guido Franco <guidofranco-@hotmail.com>
2019-03-27 18:07:50 -0300

    lectura-grupo6.java

---
+++ lectura-grupo6.java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+
+public class Bienvenida{
+
+	public static void main(String[] args){
+		System.out.println("-----RZ Software----");
+		System.out.println("");
+		System.out.println("Hola, por favor ingresá tu Nombre: ");
+		System.out.println("");
+		String entradaTeclado="";
+		Scanner entradaScanner= new Scanner(System.in);
+		entradaTeclado = entradaScanner.nextLine();
+		System.out.println("");
+		System.out.println("Bienvenid@ "+entradaTeclado+" gracias por elegirnos!!!");
+		System.out.println("");
+		System.out.println("Hasta la proxima...");
+
+
+
+	}
+}
+
+
