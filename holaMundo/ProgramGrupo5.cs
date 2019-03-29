392dbdf5caa3a8f3d0ae4d61b853cc0c375f611e
edgardo <edgarDuart2012@hotmail.com.ar>
2019-03-29 08:59:49 -0300

    se agrego archivo que realiza una lectura de texto y luego imprime en pantalla

---
+++ holaMundo/ProgramGrupo5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace holaMundo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un texto");
+            String texto;
+            texto = Console.ReadLine();
+            Console.WriteLine("El texto introducido es: " + texto);
+        }
+    }
+}
